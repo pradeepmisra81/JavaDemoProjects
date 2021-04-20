884fd9106e6d758eb0ece6b14b5003ea1ec8f18a
pradeep misra <pradeep.misra@kognitiv.com>
2021-04-20 12:55:49 +0530

    Add Multiplication Table code

---
+++ src/com/javademos/firsrtdemoproject/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.javademos.firsrtdemoproject;
+
+public class MultiplicationTable {
+	
+	void print() {
+		for(int i = 0; i <=10; i++) {
+			System.out.printf("%d * %d = %d", 5, i, 5*i).println();
+		}
+	}
+
+}
---
+++ src/com/javademos/firsrtdemoproject/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package com.javademos.firsrtdemoproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+	}
+
+}
